d24e05ac3cf5fa93a73c224ec87e13c7c9efef26
MAK064 <Mattakirk@outlook.com>
2018-02-20 17:26:29 +1100

    Added Dynamic Scaling and Fixed 'Disapearing Mario' Bug

--- MarioGravity.py
+++ MarioGravity.py
@@ -1,13 +1,14 @@
 from JMSSGraphics import *
 import math
-jmss = Graphics(width = 1200, height = 800, title = "Test Game", fps = 60)
-width = 1200
-height = 800
-M1x = 400
-M1y = 700
-M2x = 800
-M2y = 700
-G = 1/6
+
+width = int(input("Please enter your desired width: "))
+height = int(input("Please enter your desired height: "))
+jmss = Graphics(width = width, height = height, title = "Test Game", fps = 60)
+M1x = width / 3
+M1y = height - (height/5)
+M2x = width / 3 *2
+M2y = height - (height/5)
+G = 1/6        #10m/s /60s
 Time = 0
 
 @jmss.mainloop
@@ -16,8 +17,14 @@ def Game():
     global M1y, M1x, M2y, M2x, Time, G
 
     jmss.clear(0,0,0,1)
-    jmss.drawImage("mario.png" , x = M1x , y = M1y)
-    jmss.drawImage('mario.png' , x = M2x , y = M2y)
+    if (M1y >= 0) : #Checks is Mario is at y=0
+        jmss.drawImage("mario.png" , x = M1x , y = M1y)
+    else:           #Makes sure Mario doesn't disapear when he reaches y=0
+        jmss.drawImage("mario.png" , x = M1x , y = 0)
+    if (M2y >= 0) : #Checks is Mario is at y=0
+        jmss.drawImage('mario.png' , x = M2x , y = M2y)
+    else:           #Makes sure Mario doesn't disapear when he reaches y=0
+        jmss.drawImage("mario.png" , x = M2x , y = 0)
 
     M1y -= G #Linear Movement
     M2y -= (G*Time) #Non-Linear Movement
