1c58c050f23136fad8312c2fd64265ebd21f3ac2
MAK064 <Mattakirk@outlook.com>
2018-02-23 17:51:16 +1100

    Deleted

--- Cyphers/ Cypher1.py
+++ Cyphers/ Cypher1.py
@@ -1,6 +1,13 @@
-In = input("Enter Cypher: ")
+import string
+
+#In = input("Enter Cypher: ")
 Out = ""
 
+Aplhabet = string.ascii_uppercase
+Ring1 = ['Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M']
+#Ring2 = []
+#Ring3 = []
 
-
-print(Out)
+Out = Aplhabet
+print (Alphabet)
+#print(Out)
