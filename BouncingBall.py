09092e4c2f88c746c8f73692bd66ec7af8828d38
MAK064 <Mattakirk@outlook.com>
2018-03-01 12:28:57 +1100

    Added momentum variable

--- BouncingBall.py
+++ BouncingBall.py
@@ -1,30 +1,40 @@
 from JMSSGraphics import *
 
+# Dimentions
 width = 1200
 height = 800
 fps = 60
 
 jmss = Graphics(width = width, height = height, title = "Bouncing Ball", fps = fps)
 
+#Setting Initail Variables
 BallX = 1/3*width
 BallY = 2/3*height
 TFall = 0
 direction = 1
+momentum = 5
 G = -2/6
 
 @jmss.mainloop
 def simulation():
-    global BallX, BallY, TFall, G, width, direction
+    global BallX, BallY, TFall, G, width, direction, momentum
 
     jmss.clear(0,0,0,1)
-    if 0 <= BallY:
+
+    #Draws Mario
+    if 0 <= BallY < (height - 64):
         jmss.drawImage("ball.png", x = BallX, y = BallY)
         BallX += direction*5
         BallY += G*TFall
+    elif BallY >= (height - 64):
+        jmss.drawImage("ball.png", x = BallX, y = (height - 64))
+        TFall = TFall*(-1)
+        BallX += direction * momentum
+        BallY += G*TFall
     else:
         jmss.drawImage("ball.png", x = BallX, y = 0)
-        TFall = TFall*(-0.98)
-        BallX += direction *5
+        TFall = TFall*(-1.02)
+        BallX += direction * momentum
         BallY += G*TFall
     if (0 >= BallX or BallX >= (width - 64)):
         direction = direction*(-1)
