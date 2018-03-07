58c49ce0e9c6ae4365876b88b60fa6c1b24c27f2
MAK064 <Mattakirk@outlook.com>
2018-03-07 13:33:46 +1100

    Change Control Flow

--- BouncingBall.py
+++ BouncingBall.py
@@ -14,7 +14,7 @@ BallY = random.randint(1, (height - 65))
 
 momentum = random.uniform(0, 10)
 G = random.uniform(1, -1)
-TFall = random.uniform(-100, 0)
+TFall = random.uniform(-50, 50)
 
 if (random.randint(0, 1) == 1):
     direction = 1
@@ -28,33 +28,32 @@ print (G)
 print (TFall)
 print ("(" + str(BallX) + ", " + str(BallY) + ")")
 
+#Loads ball into memory
 ball = jmss.loadImage("ball.png")
 
+#The main loop that is executed every frame
 @jmss.mainloop
 def simulation():
     global BallX, BallY, TFall, G, width, direction, momentum
 
     jmss.clear(0,0,0,1)
 
-    #Does logic and draws mario when inbetween top and bottom of screen
+    #Does logic for the ball when inbetween top and bottom of screen
     if 0 <= BallY < (height - 64):
-        jmss.drawImage( ball , x = BallX, y = BallY)
         BallX += direction*momentum
         BallY += G*TFall
 
-    #Does logic and draws mario when above top of screen
-    elif BallY >= (height - 64):
-        jmss.drawImage( ball , x = BallX, y = (height - 64))
-        TFall = TFall*(-0.5)
+    #Does logic for the ball when above top of screen
+    elif BallY > (height - 64):
+        TFall = TFall*(-0.98)
         BallX += direction * momentum
-        BallY = height - 64 + G*TFall
+        BallY = height - 65
 
-    #Does logic and draws mario when below screen
+    #Does logic for the ball when below screen
     else:
-        jmss.drawImage( ball , x = BallX, y = 0)
-        TFall = TFall*(-1)
+        TFall = TFall*(-0.98)
         BallX += direction * momentum
-        BallY += G*TFall
+        BallY = 1
 
     #Logic for x direcion reversal
     if (0 >= BallX or BallX >= (width - 64)):
@@ -64,6 +63,9 @@ def simulation():
     jmss.drawText("Ball's Position is: (" + str(int(BallX)) + ", " + str(int(BallY)) + ")" , x = 0, y = 0)
     jmss.drawText("G = " + str(G/(2/6))  , x = width - 68, y = 0)
     jmss.drawText("Ball's sideways momentum is ~ " + str(int(momentum)) , x = width - 205, y = 12)
+
+    #Draws the ball from memory
+    jmss.drawImage( ball , x = BallX, y = BallY)
 
     #Increment timefalling
     TFall += 1
