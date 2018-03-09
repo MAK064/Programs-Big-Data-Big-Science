3c7950b64e65ff55187e7c8fb4f589ae06971ed3
MAK064 <Mattakirk@outlook.com>
2018-03-09 13:25:36 +1100

    Attempt Colision

--- Pong Game/Pong_Game.py
+++ Pong Game/Pong_Game.py
@@ -15,8 +15,8 @@ ydir = 1
 p1x = 1/12*width
 p1y = 1/2*height
 
-#p2x =
-#p2y =
+p2x = 11/12*width
+p2y = 1/2*height
 
 
 Ball = jmss.loadImage("ball.png")
@@ -24,25 +24,31 @@ Paddle = jmss.loadImage("Pong Paddle.jpg")
 
 @jmss.mainloop
 def Pong ():
-    global bax, bay, xdir, ydir, height, p1y, p1x
+    global bax, bay, xdir, ydir, height, p1y, p1x, p2y, p2x
 
     if jmss.isKeyDown(KEY_W):
         p1y += 3
     if jmss.isKeyDown(KEY_S):
         p1y -= 3
+    if jmss.isKeyDown(KEY_I):
+        p2y += 3
+    if jmss.isKeyDown(KEY_K):
+        p2y -= 3
 
-
-    if (bax > width - 64 or bax < 0 or (p1x <= bax <= (p1x + 35) and p1y <= bay <= (p1y + 125))):
+    if (bax == (1/12*width)-32 and (p1y - 32) <= bax <= (p1y + 64)):
         xdir = -xdir
 
+    if (bax > width - 64 or bax < 0):
+        xdir = -xdir
     if (bay > height - 64 or bay < 0):
         ydir = -ydir
 
     bax += 5*xdir
-    bay += 5*ydir
+    #bay += 5*ydir
 
     jmss.clear()
     jmss.drawImage(Ball, x = bax, y = bay)
-    jmss.drawImage(Paddle, x = p1x, y = p1y)
+    jmss.drawImage(Paddle, x = p1x, y = p1y + 64)
+    jmss.drawImage(Paddle, x = p2x, y = p2y + 64)
 
 jmss.run()
