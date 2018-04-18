b89c95afb9f17212ae2877b5dbea23fd9304529c
MAK064 <Mattakirk@outlook.com>
2018-04-18 11:53:53 +1000

    Tidy Up 2

---
+++ Big Data Challenges/Loops/Parrabola.py
@@ -0,0 +1,21 @@
+from JMSSGraphics import *
+import math
+
+width = 1200
+height = 800
+fps = 60
+
+jmss = Graphics(width = width, height = height, title = "Bouncing Ball", fps = fps)
+
+ball = jmss.loadImage("ball.png")
+
+ball_pos = [0,0]
+
+@jmss.mainloop
+def Parabola():
+    global ball_pos
+    while ball_pos[0] <= 1200:
+        ball_pos[1] = math.sin(ball_pos[0])*64
+        jmss.drawImage(ball, ball_pos[0], ball_pos[1] + 300)
+        ball_pos[0] += 32
+jmss.run()
---
+++ Testing.py
@@ -0,0 +1,6 @@
+age = 16
+out = ""
+while age >= 1:
+    out += "*"
+    age -= 1
+print(out)
