6f5d097681fbaacafca4fd727e99ac03e737bb1a
MAK064 <Mattakirk@outlook.com>
2018-05-01 17:12:54 +1000

    ZMesh V1

--- Graphics and Sims/ZMesh.py
+++ Graphics and Sims/ZMesh.py
@@ -8,26 +8,21 @@ fps = 60
 jmss = Graphics(width = width, height = height, title = "Graphics", fps = fps)
 ball = jmss.loadImage("ball.png")
 
-CPos = [0,0]
 ZMesh = []
+
+#Prerequisits
+for x in range(0,120):
+    for y in range(0,80):
+        z = (x**2)+(y**2)/(10**7)
+        ZMesh.append([x*10,y*10,z])
 
 @jmss.mainloop
 def Test():
     global CPos, ZMesh, width, height
     jmss.clear(0,0,0,1)
-    CPos[0] = 0
-    CPos[1] = 0
-    
-    while CPos[0] <= width:
 
-        while CPos[1] <= height:
-
-            z = (CPos[0]**2)+(CPos[1]**2)/(10**7)
-            ZMesh.append([CPos[0],CPos[1],z])
-            CPos[1] +=10
-
-        CPos[0] += 10
-
-    print(ZMesh)
+    for x in range(0,120):
+        for y in range(0,80):
+            jmss.drawText()
 
 jmss.run()
