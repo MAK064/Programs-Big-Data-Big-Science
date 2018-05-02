ca2f20e99a7ba90458c4839c0d7fdf0a62fd1225
MAK064 <Mattakirk@outlook.com>
2018-05-02 17:16:07 +1000

    Fixed ZMesh

--- Graphics and Sims/ZMesh.py
+++ Graphics and Sims/ZMesh.py
@@ -1,5 +1,5 @@
 from JMSSGraphicsV12 import *
-import math
+import math, random
 
 width = 1200
 height = 800
@@ -10,12 +10,6 @@ ball = jmss.loadImage("ball.png")
 
 ZMesh = []
 
-#Prerequisits
-for x in range(0,120):
-    for y in range(0,80):
-        z = (x**2)+(y**2)/(10**7)
-        ZMesh.append([x*10,y*10,z])
-
 @jmss.mainloop
 def Test():
     global CPos, ZMesh, width, height
@@ -23,6 +17,19 @@ def Test():
 
     for x in range(0,120):
         for y in range(0,80):
-            jmss.drawText()
+            z = (((x*10-600)**2)+((y*10-400)**2))/(10**-0.5)
+            ZMesh.append([x*10,y*10,z])
+            if z <= 10**5:
+                jmss.drawPixel((1,0,0,1),x*10,y*10)
+            elif 10**5 < z <= 2*10**5:
+                jmss.drawPixel((1,1,0,1),x*10,y*10)
+            elif 2*10**5 < z <= 4*10**5:
+                jmss.drawPixel((0,1,0,1),x*10,y*10)
+            elif 4*10**5 < z:
+                jmss.drawPixel((0,1,1,1),x*10,y*10)
+            else:
+                print("error")
+
+    #print(str(ZMesh[random.randint(0,120)*80+random.randint(0,80)]))
 
 jmss.run()
